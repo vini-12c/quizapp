5cc330e0d8ec5d50b0e8d41e20c53789738f5311
vinicyus rodrigues <vinicyus15062006@gmail.com>
2024-05-29 18:00:24 +0000

    primeiros ajustes no tema javascript

---
+++ script.js
@@ -0,0 +1,33 @@
+let tema = "claro"
+
+const botaoTema = document.querySelector(".tema button")
+botaoTema.addEventListener("click", trocaTema)
+
+const body = document.querySelector("body")
+
+function trocaTema() {
+    if (localStorage.getItem("tema")){
+        tema = localStorage.getItem("tema")
+    }
+
+    if (tema === "claro"){
+        body.classList.add("escuro")
+        localStorage.setItem("tema", "escuro")
+        botaoTema
+    }
+    else {
+        body.classList.remove("escuro")
+        localStorage.setItem("tema", "claro")
+    }
+}
+
+function verificarTema(){
+    if (localStorage.getItem("tema")){
+        tema = localStorage.getItem("tema")
+    }
+
+    if (tema === "escuro"){
+        body.classList.add("escuro")
+    }
+}
+verificarTema()
