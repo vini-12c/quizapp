4c9c91e676290b96ce5de5faef839758a1e6b837
vinicyus rodrigues <vinicyus15062006@gmail.com>
2024-06-19 23:21:58 +0000

    ajustes da função selecionar assunto para funcionar em todos os dispositivos

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ botoesAssunto.forEach(botao => {
 })
 
 function selecionarAssunto(evento){
-    const assunto = evento.target.innerText
+    const classeBotao = evento.target.classname
+    const assunto = document.querySelector(`.${classeBotao} span`).innerText
     localStorage.setItem("assunto", assunto)
     window.location.href = "./pages/quiz/quiz.html"
 }
